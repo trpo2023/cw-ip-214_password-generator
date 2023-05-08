a24c432e49a638a9db54a9f6a3894f36d3dd5018
Maksim <2dearr2@gmail.com>
2023-05-08 16:14:41 +0700

    Edit main.c

--- src/pwgen/main.c
+++ src/pwgen/main.c
@@ -3,7 +3,10 @@
 #include "psw.h"
 int main()
 {
+    char* message = "Your password: ";
+    puts(message);
     char** words_array = array_words();
     parsmake(words_array);
     pass_string(words_array);
+    return 0;
 }
