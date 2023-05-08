713abdbb01ed49e3fbf28eba0d1b4e1caf7334a9
Maksim <2dearr2@gmail.com>
2023-05-08 15:53:08 +0700

    Edit codestyle

--- src/libpwgen/psw.c
+++ src/libpwgen/psw.c
@@ -4,7 +4,7 @@
 
 void pass_string(char** words_array)
 {
-    char *psw_str = parsmake(words_array);
+    char* psw_str = parsmake(words_array);
     while (strlen(psw_str) >= strlen(words_array[0]) - 2) {
         psw_str = parsmake(words_array);
     }
