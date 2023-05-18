92b91b97e6a989fee629211533becea9b9c311ae
Maksim <2dearr2@gmail.com>
2023-05-18 22:13:16 +0700

    Added the ability to select data input

--- src/libpwgen/psw.h
+++ src/libpwgen/psw.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 void pass_string(char**, int);
 char* edit_string(char*, int);
 void add_num(char*, char*, int);
--- src/pwgen/main.c
+++ src/pwgen/main.c
@@ -4,22 +4,54 @@
 #include "psw.h"
 int main(int argc, char* argv[])
 {
-    int len_pas = 0, val_pas = 0;
-    if ((argc > 3 && argc != 1) || argc == 2) {
-        printf("Usage: %s LEN_PAS val_pas\n", argv[0]);
-        exit(1);
-    }
-    if (argc == 1) {
+    srand(time(NULL));
+    char choice;
+    printf("Do you want enter input data? y/n\n");
+    scanf("%c", &choice);
+    if (choice == 'y') {
+        if ((argc > 3 && argc != 1) || argc == 2) {
+            printf("Usage: %s LEN_PAS val_pas\n", argv[0]);
+            exit(1);
+        }
+        int len_pas = 0, val_pas = 0;
         printf("Enter length password: ");
         scanf("%d", &len_pas);
         printf("Enter amount password: ");
         scanf("%d", &val_pas);
+        char** words_array = array_words();
+        for (int i = 0; i < val_pas; i++) {
+            pass_string(words_array, len_pas);
+        }
     } else {
-        len_pas = atoi(argv[1]);
-        val_pas = atoi(argv[2]);
+        FILE* f;
+        f = fopen("params.txt", "r");
+        if (f == NULL) {
+            printf("Error: cannot open file.\n");
+            return 1;
+        }
+        fseek(f, 0L, SEEK_END);
+        if (ftell(f) == 0) {
+            printf("Params not found in file. Please enter them through "
+                   "terminal.\n");
+            int len_pas, val_pas;
+            printf("Enter length password: ");
+            scanf("%d", &len_pas);
+            printf("Enter amount password: ");
+            scanf("%d", &val_pas);
+            char** words_array = array_words();
+            for (int i = 0; i < val_pas; i++) {
+                pass_string(words_array, len_pas);
+            }
+        } else {
+            rewind(f);
+            int len_pas, val_pas;
+            fscanf(f, "%d %d", &len_pas, &val_pas);
+            char** words_array = array_words();
+            for (int i = 0; i < val_pas; i++) {
+                pass_string(words_array, len_pas);
+            }
+        }
+        fclose(f);
     }
-    char** words_array = array_words();
-    for (int i = 0; i < val_pas; i++) {
-        pass_string(words_array, len_pas);
-    }
+    return 0;
 }
