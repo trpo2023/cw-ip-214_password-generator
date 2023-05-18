fb9a01ae3181c9cee16cc11dac034828d965347b
Maksim <2dearr2@gmail.com>
2023-05-18 17:30:56 +0700

    Edit psw.c and added variable len_pas in all function

--- src/libpwgen/psw.h
+++ src/libpwgen/psw.h
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void pass_string(char**);
-char* edit_string(char*);
-void add_num(char*, char*);
-void insert_word(char*, char*, int);
-void add_special(char*, char*);
+void pass_string(char**, int);
+char* edit_string(char*, int);
+void add_num(char*, char*, int);
+void insert_word(char*, char*, int, int);
+void add_special(char*, char*, int);
--- src/pwgen/main.h
+++
@@ -1,4 +0,0 @@
-#include "fileread.h"
-#include "parser.h"
-#include "psw.h"
-extern int LEN_PAS;
