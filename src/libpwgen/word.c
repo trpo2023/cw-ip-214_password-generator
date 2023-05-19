a6132be587962e519d0f0855191b5e15b24dabc8
Maksim <2dearr2@gmail.com>
2023-05-19 20:29:20 +0700

    Add new file for function choice word

---
+++ src/libpwgen/word.c
@@ -0,0 +1,15 @@
+#include <psw.h>
+#include <word.h>
+void pass_with_word(char* word, int len_pas)
+{
+    char* psw_str = word;
+    int len_psw_str = strlen(psw_str);
+    while (len_psw_str + 2 > len_pas) {
+        printf("The length of your word is too long, please enter a shorter "
+               "one: ");
+        scanf("%s", word);
+        psw_str = word;
+        len_psw_str = strlen(psw_str);
+    }
+    edit_string(psw_str, len_pas);
+}
